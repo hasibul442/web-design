87dd9cfb18465d2a28186bee67c710152dc3bd66
Mohammad Hasibul Hasan <hasibulhasan442@gmail.com>
2020-12-31 21:24:17 +0600

    Add files via upload

---
+++ Design-3/assets/js/main.js
@@ -0,0 +1,82 @@
+(function($) {
+  
+  "use strict";  
+
+  $(window).on('load', function() {
+
+  /*Page Loader active
+    ========================================================*/
+    $('#preloader').fadeOut();
+
+  // Sticky Nav
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 200) {
+            $('.scrolling-navbar').addClass('top-nav-collapse');
+        } else {
+            $('.scrolling-navbar').removeClass('top-nav-collapse');
+        }
+    });
+
+    /* ==========================================================================
+       countdown timer
+       ========================================================================== */
+     jQuery('#clock').countdown('2021/06/21',function(event){
+      var $this=jQuery(this).html(event.strftime(''
+      +'<div class="time-entry days" style="background-color:#48bfe3"><span>%-D</span> Days</div> '
+      +'<div class="time-entry hours"style="background-color:#56cfe1"><span>%H</span> Hours</div> '
+      +'<div class="time-entry minutes"style="background-color:#64dfdf"><span>%M</span> Minutes</div> '
+      +'<div class="time-entry seconds"style="background-color:#72efdd"><span>%S</span> Seconds</div> '));
+    });
+
+    /* slicknav mobile menu active  */
+    $('.mobile-menu').slicknav({
+        prependTo: '.navbar-header',
+        parentTag: 'liner',
+        allowParentLinks: true,
+        duplicate: true,
+        label: '',
+      });
+
+      /* WOW Scroll Spy
+    ========================================================*/
+     var wow = new WOW({
+      //disabled for mobile
+        mobile: false
+    });
+    wow.init();
+
+    /* Nivo Lightbox 
+    ========================================================*/
+    $('.lightbox').nivoLightbox({
+        effect: 'fadeScale',
+        keyboardNav: true,
+      });
+
+    // one page navigation 
+    $('.navbar-nav').onePageNav({
+            currentClass: 'active'
+    }); 
+
+    /* Back Top Link active
+    ========================================================*/
+      var offset = 200;
+      var duration = 500;
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+          $('.back-to-top').fadeIn(400);
+        } else {
+          $('.back-to-top').fadeOut(400);
+        }
+      });
+
+      $('.back-to-top').on('click',function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: 0
+        }, 600);
+        return false;
+      });
+
+  });      
+
+}(jQuery));
